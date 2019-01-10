b5872f465bd0e0bacbd8fa578085d67a78822da4
Jocke <joakim-asplund@hotmail.com>
2019-01-10 01:53:19 +0100

    Add project files.

---
+++ Zinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zinder.Startup))]
+namespace Zinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
